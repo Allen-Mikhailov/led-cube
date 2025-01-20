2485f2bee9731277097af147aff15707c443e303
Allen Mikhailov <slinkyshelf8@gmail.com>
2025-01-20 01:17:01 -0600

    Most features of the web tool work, only download is left to be completed

--- pattern_creator/index.js
+++ pattern_creator/index.js
@@ -1,23 +1,63 @@
 const x_input = document.getElementById("x-dim")
 const y_input = document.getElementById("y-dim")
 const z_input = document.getElementById("z-dim")
+
+const z_axis_input = document.getElementById("z-input")
+const next_z = document.getElementById("next-z")
+const prev_z = document.getElementById("prev-z")
+
 const slot_grid = document.getElementById("grid-container")
+
+const phase_count_input = document.getElementById("phase-count-input")
 const phase_input = document.getElementById("phase-input")
+const next_phase = document.getElementById("next-phase")
+const prev_phase = document.getElementById("prev-phase")
 
 const active_color = "#fff"
 const unactive_color = "#000"
 
-let dimensions = [1, 1, 1]
-let phases = []
-let selected_phase = 0
+let dimensions = [3, 3, 3]
 let z_axis = 0
+
+let phase_count = 1
+let selected_phase = 0
+
+
+let phases = [] // data
+
 let slots = {}
 
-
+function clamp(v, min, max)
+{
+    return Math.min(Math.max(v, min), max)
+}
 
 function update_style(slot_element, value)
 {
     slot_element.style.backgroundColor = value == 1?active_color:unactive_color
+}
+
+// this is some of the worst code I have ever written
+function touch_slot(x, y)
+{
+    // phase check
+    const phase = phases[selected_phase] || []
+    phases[selected_phase] = phase
+
+    // slice check
+    const z_slice = phase[z_axis] || []
+    phase[z_axis] = z_slice
+
+    // row check
+    const row = z_slice[y] || []
+    z_slice[y] = row
+
+    // value check
+    value = row[x] || 0
+    row[x] = value
+
+    return value
+        
 }
 
 function slot_update()
@@ -27,47 +67,43 @@ function slot_update()
         for (let y = 0; y < dimensions[1]; y++)
         {
             const div = slots[`${x},${y}`]
-            update_style(div, phases[selected_phase][y][x])
+            update_style(div, touch_slot(x, y))
         }
     }
+}
+
+function update_phase(new_phase)
+{
+    new_phase = clamp(new_phase, 0, phase_count-1)
+    selected_phase = new_phase
+    phase_input.value = new_phase+1
+    slot_update()
 }
 
 phase_input.onchange = () => {
     try {
-        selected_phase = parseFloat(phase_input.value)
-        slot_update()
+        update_phase(parseInt(phase_input.value)-1)
     } catch(e) {
         console.log(e)
     }
-    
 }
-phase_input.value = 0
 
-function compile()
+function update_z_slice(new_z)
 {
-    for (let x = 0; x < dimensions[0]; x++)
-    {
-        for (let y = 0; y < dimensions[1]; y++)
-        {
-            const div = document.createElement("div")
-            div.className = "slot"
+    new_z = clamp(new_z, 0, dimensions[2]-1)
+    z_axis_input.value = new_z+1
+    z_axis = new_z
+    slot_update()
+}
 
-            div.style.width = `calc(${size}% - 1px)`
-            div.style.height = `calc(${size}% - 1px)`
-            div.style.left = `${size*x}%`
-            div.style.top = `${size*y}%`
-            div.onclick = () => {
-                console.log("click", phases[selected_phase][y][x])
-                const value = phases[selected_phase][y][x] == 0?1:0
-                phases[selected_phase][y][x] = value
-                update_style(div, value)
-            }
-            update_style(div, phases[selected_phase][y][x])
-            slots[`${x},${y}`] = div
-            slot_grid.appendChild(div)
-        }
+z_axis_input.onchange = () => {
+    try {
+        update_phase(parseInt(z_axis_input.value)-1)
+    } catch(e) {
+        console.log(e)
     }
 }
+
 
 function dimension_update()
 {
@@ -79,25 +115,9 @@ function dimension_update()
         ]
         console.log("new dimensions", dimensions)
 
-        // making sure the dimensions are filled
-        for (let z = 0; z < dimensions[2]; z++) {
-            phases[z] = phases[z] || []
-            const phase = phases[z]
-            for (let y = 0; y < dimensions[1]; y++) {
-                phase[y] = phase[y] || []
-                const row = phase[y]
-                for (let x = 0; x < dimensions[1]; x++)
-                {
-                    row[x] = row[x] || 0
-                }
-            }
-        }   
-
         const size = 100/Math.max(dimensions[0], dimensions[1])
 
-        slot_grid.replaceChildren()
-
-        
+        slot_grid.replaceChildren();
         for (let x = 0; x < dimensions[0]; x++)
         {
             for (let y = 0; y < dimensions[1]; y++)
@@ -109,19 +129,17 @@ function dimension_update()
                 div.style.height = `calc(${size}% - 1px)`
                 div.style.left = `${size*x}%`
                 div.style.top = `${size*y}%`
+                
                 div.onclick = () => {
-                    console.log("click", phases[selected_phase][y][x])
-                    const value = phases[selected_phase][y][x] == 0?1:0
-                    phases[selected_phase][y][x] = value
+                    const value = touch_slot(x, y) == 0?1:0
+                    phases[selected_phase][z_axis][y][x] = value
                     update_style(div, value)
                 }
-                update_style(div, phases[selected_phase][y][x])
+                update_style(div, touch_slot(x, y))
+                slots[`${x},${y}`] = div
                 slot_grid.appendChild(div)
             }
         }
-
-        
-        console.log("replaced")
     } catch (e) {
         console.log(e)
     }
@@ -136,5 +154,17 @@ z_input.value = dimensions[2]
 x_input.onchange = dimension_update
 y_input.onchange = dimension_update
 z_input.onchange = dimension_update
+
+next_z.onclick = () => update_z_slice(z_axis+1)
+prev_z.onclick = () => update_z_slice(z_axis-1)
+
+next_phase.onclick = () => update_phase(selected_phase+1)
+prev_phase.onclick = () => update_phase(selected_phase-1)
+
+phase_input.value = 1
+z_axis_input.value = 1
+
+phase_count_input.value = phase_count
+phase_count_input.onchange = () => phase_count = parseInt(phase_count_input.value)
 
 dimension_update()
